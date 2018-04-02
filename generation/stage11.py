8ae4ad6c69ffb9c7bcec73afaacbb3e37ad99c3c
fisent <lukzieniewicz@gmail.com>
2018-04-03 01:19:55 +0200

    Stage 11 in progress

--- generation/stage11.py
+++ generation/stage11.py
@@ -1,3 +1,6 @@
+from docx.shared import Pt
+
+from generation import Project
 
 
 class Stage11(object):
@@ -6,4 +9,38 @@ class Stage11(object):
         self.erd = erd
 
     def build(self, document):
-        pass
+        header = document.add_paragraph()
+        header.add_run('11. Definicje schematów relacji i przykładowe dane w poszczególnych tabelach').font.size = Pt(Project.HEADER_SIZE)
+        header.add_run().add_break()
+
+        for entity in self.erd.entities:
+            relation_paragraph = document.add_paragraph()
+            relation_paragraph.add_run('REL/' + '{0:03}'.format(entity.id) + entity.name_plural + '/' + entity.name_singular.upper()).bold = True
+            relation_paragraph.add_run().add_break()
+            relation_paragraph.add_run('Opis schematu relacji:')
+            relation_paragraph.add_run().add_break()
+
+            table = document.add_table(rows = len(entity.attributes) + len(entity.foreign_keys) + 1, cols = 10)
+            hdr_cells = table.rows[0].cells
+            runs = []
+            runs.append(hdr_cells[0].paragraphs[0].add_run('Nazwa atrybutu'))
+            runs.append(hdr_cells[1].paragraphs[0].add_run('Dziedzina'))
+            runs.append(hdr_cells[2].paragraphs[0].add_run('Maska'))
+            runs.append(hdr_cells[3].paragraphs[0].add_run('OBL'))
+            runs.append(hdr_cells[4].paragraphs[0].add_run('Wart. Dom.'))
+            runs.append(hdr_cells[5].paragraphs[0].add_run('Ograniczenia'))
+            runs.append(hdr_cells[6].paragraphs[0].add_run('Unikalność'))
+            runs.append(hdr_cells[7].paragraphs[0].add_run('Klucz'))
+            runs.append(hdr_cells[8].paragraphs[0].add_run('Referencje'))
+            runs.append(hdr_cells[9].paragraphs[0].add_run('Źródło danych'))
+            for run in runs:
+                run.font.size = Pt(10)
+                run.bold = True
+            row_counter = 1
+            for attribute in entity.attributes:
+                row = table.rows[row_counter].cells
+                row[0].text = attribute.name
+                row_counter += 1
+
+
+
